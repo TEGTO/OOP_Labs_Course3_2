c2bb3ee231eb9a88cb10e3bbc9d8af24c23d9809
Eugene <pshonovskij@gmail.com>
2024-04-20 18:11:30 +0300

    3: Added lab2 & description

--- Lab2/airport-management-backend/src/main/resources/init.sql
+++ Lab2/airport-management-backend/src/main/resources/init.sql
@@ -1,23 +1,3 @@
-CREATE TABLE IF NOT EXISTS crew_members(
-    id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    surname VARCHAR(255) NOT NULL,
-    position VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS flights(
-    id BIGSERIAL PRIMARY KEY,
-    departure_from VARCHAR(255) NOT NULL,
-    destination VARCHAR(255) NOT NULL,
-    departure_time TIMESTAMP(6) NOT NULL,
-    arrival_time TIMESTAMP(6) NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS crew_members_flights(
-    id BIGSERIAL PRIMARY KEY,
-    fk_crew_member_id BIGINT REFERENCES crew_members(id) ON DELETE CASCADE,
-    fk_flight_id BIGINT REFERENCES flights(id) ON DELETE CASCADE
-)
 INSERT INTO crew_members (id, name, position, surname) VALUES (1, 'Roman', 'PILOT', 'Vasin');
 INSERT INTO crew_members (id, name, position, surname) VALUES (2, 'Ivan', 'PILOT', 'Prigoda');
 INSERT INTO crew_members (id, name, position, surname) VALUES (3, 'Stepan', 'OPERATOR', 'Ilchuk');
